2dce85e4bf39f6c7c4cf9560c65b4dd1f2a92157
邵家琦 <tzpcsjq@msn.cn>
2018-05-02 22:34:17 +0800

    添加ignore，第一次编译通过

--- main.go
+++ main.go
@@ -24,7 +24,11 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
 
 	//start server
 	if err := http.ListenAndServe(":8888", nil); err != nil {
