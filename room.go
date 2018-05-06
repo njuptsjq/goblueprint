e362e1f7e2d36bdea41aeec5df48b74e1a629759
邵家琦 <tzpcsjq@msn.cn>
2018-05-06 15:37:20 +0800

    修改后台信息传输为json格式

--- room.go
+++ room.go
@@ -5,13 +5,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/stretchr/objx"
+
 	"github.com/gorilla/websocket"
 )
 
 type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other clinets
-	forward chan []byte
+	forward chan *message
 	// join  is a channel for clients wishing to join
 	join chan *client
 	// leave is a channel for leaving
@@ -38,7 +40,7 @@ func (r *room) run() {
 			// forward message
 			for client := range r.clients {
 				client.send <- msg
-				r.tracer.Trace("-- sent to client")
+				r.tracer.Trace("-- sent to client", msg.Message)
 			}
 
 		}
@@ -58,10 +60,16 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		log.Fatal("ServeHTTP", err)
 		return
 	}
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("Failed to get auth cookie:", err)
+		return
+	}
 	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
@@ -71,7 +79,7 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
